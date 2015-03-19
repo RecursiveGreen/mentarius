9167a6ba39740de3a0be52f4f0eab7221f1ab45c
Josh Washburne <recursive.green@gmail.com>
2015-03-19 14:24:02 -0400

    Initial commit for plugin interfaces.

---
+++ plugin.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+class PluginRegistry(type):
+    plugins = []
+    def __init__(cls, name, bases, attrs):
+        if name != 'Plugin':
+            PluginRegistry.plugins.append(cls)
+
+
+class StoragePlugin(object, metaclass=PluginRegistry):
+    def __init__(self, journal=None):
+        self.journal = journal
+
+    def new(self):
+        ''' Creates and initializes a new journal within storage. '''
+        return None
+
+    def load(self):
+        ''' Loads a journal from storage. '''
+        return None
+
+    def save(self):
+        ''' Saves a journal to storage. '''
+        return None
