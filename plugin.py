f48b40cacb940f7ee91003d56b97bb6ce72487a7
Josh Washburne <recursive.green@gmail.com>
2015-03-24 11:40:36 -0400

    Discovering plugins now possible.

--- plugin.py
+++ plugin.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import importlib
 import os
 
 class PluginRegistry(type):
+    base_classes = ['StoragePlugin',]
     plugins = []
     def __init__(cls, name, bases, attrs):
-        if name != 'Plugin':
+        if name not in PluginRegistry.base_classes:
             PluginRegistry.plugins.append(cls)
 
 
@@ -25,3 +27,19 @@ class StoragePlugin(object, metaclass=PluginRegistry):
     def save(self):
         ''' Saves a journal to storage. '''
         return None
+
+
+def discover_plugins(dirs):
+    ''' Find plugin classes contained within a list of directories. Returns
+        a list of loaded plugins.
+    '''
+    for dirname in dirs:
+        for dpath, dnames, fnames in os.walk(dirname):
+            for fname in fnames:
+                if fname != '__init__.py' and fname.endswith('py'):
+                    path = dpath.split(os.sep)
+                    path = filter(None, path[path.index(dirname):])
+                    mod_dir = '.'.join(path)
+                    importlib.import_module(mod_dir + '.' + fname[:-3])
+
+    return PluginRegistry.plugins
